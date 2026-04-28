6a4d3c0fe54050baac38a9129a98486c14b79eaf
divyatamarani <divyatamarani@gmail.com>
2026-04-28 06:16:27 +0530

    UC10: Detect draw condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,50 +1,33 @@
+/**
+ * UC10 – Detect Draw Condition
+ */
 public class TicTacToe {
 
     static char[][] board = {
-            {'X', 'X', 'X'},
-            {'-', '-', '-'},
-            {'-', '-', '-'}
+            {'X', 'O', 'X'},
+            {'X', 'O', 'O'},
+            {'O', 'X', 'X'}
     };
 
     public static void main(String[] args) {
 
-        System.out.println(hasWon('X'));
+        System.out.println("Is Draw: " + isDraw());
     }
 
-    // UC9 – Check Winning Condition
-    public static boolean hasWon(char symbol) {
+    // Method to check draw
+    public static boolean isDraw() {
 
-        // Check rows
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                board[i][1] == symbol &&
-                board[i][2] == symbol) {
-                return true;
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+
+                // If any empty cell exists → NOT draw
+                if (board[r][c] == '-') {
+                    return false;
+                }
             }
         }
 
-        // Check columns
-        for (int i = 0; i < 3; i++) {
-            if (board[0][i] == symbol &&
-                board[1][i] == symbol &&
-                board[2][i] == symbol) {
-                return true;
-            }
-        }
-
-        // Check diagonals
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol) {
-            return true;
-        }
-
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
+        // No empty cells → draw
+        return true;
     }
 }
